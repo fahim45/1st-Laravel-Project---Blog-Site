ec6f0e87ca424f84212a62ff85e3cd1c83d1919b
fahim45 <fahim149004@gmail.com>
2017-10-05 11:50:19 +0600

    first laravel project

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    public function index(){
+        return view('front.home.home-content');
+    }
+    public function support(){
+        return view('front.support.support-content');
+    }
+    public function aboutUs(){
+        return view('front.about-us.about-content');
+    }
+    public function blog(){
+        return view('front.blog.blog-content');
+    }
+    public function contactUs(){
+        return view('front.contact-us.contact-content');
+    }
+
+}
